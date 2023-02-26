903bf9004f29b3d53aa2c4d11beaa8e34f537995
Matthew Vine <mvine13@gmail.com>
2023-02-26 14:49:53 -0500

    initial commit

---
+++ Recipe_translator/Recipe_translatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_translatorApp.swift
+//  Recipe_translator
+//
+//  Created by Matthew Vine on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_translatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Recipe_translatorUITests/Recipe_translatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Recipe_translatorUITestsLaunchTests.swift
+//  Recipe_translatorUITests
+//
+//  Created by Matthew Vine on 2/26/23.
+//
+
+import XCTest
+
+final class Recipe_translatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
